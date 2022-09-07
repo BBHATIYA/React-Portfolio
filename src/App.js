0a30d41a05cb949166c425032b2505d8ac199572
BBHATIYA <BHATIYABHAVESH03@GMAIL.COM>
2022-09-07 14:26:58 +0300

    port

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./components/Home/Home.js";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
-import Resume from "./components/Resume/ResumeNew";
+// import Resume from "./components/Resume/ResumeNew";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./style.css";
 // import "./App.css";
@@ -31,7 +31,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/project" element={<Projects />} />
             <Route path="/about" element={<About />} />
-            <Route path="/resume" element={<Resume />} />
+            {/* <Route path="/resume" element={<Resume />} /> */}
           </Routes>
           <Footer />
         </div>
