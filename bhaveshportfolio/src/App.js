eeeab933a81483fc53ca15a0a6b8195d10f10c83
BBHATIYA <BHATIYABHAVESH03@GMAIL.COM>
2022-09-07 19:35:42 +0300

    adding files

--- bhaveshportfolio/src/App.js
+++ bhaveshportfolio/src/App.js
@@ -1,11 +1,10 @@
-
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello</h1>
+        <h1>Hello 1</h1>
       </header>
     </div>
   );
